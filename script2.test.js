3e254821c30b88dd7ccee7130bd86be70f545f31
saroj <sarojsitaula@gmail.com>
2021-06-22 16:34:58 +0300

    mocking api calls test

--- script2.test.js
+++ script2.test.js
@@ -8,3 +8,22 @@ it("get people from swapi api", () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+it("getPeople returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 89,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("http://swapi.py4e.com/api/people");
+    expect(data.count).toEqual(89);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
